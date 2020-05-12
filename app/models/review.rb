5a40d5583b244f6723f20dea916332a0f2f42016
Matt Gordon <mattg1520@gmail.com>
2020-05-12 13:38:26 +0300

    Added restaurant and review controllers

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
+  RANGE = %w(0..5)
   belongs_to :restaurant
+
+  validates :restaurant_id, :content, :rating, presence: true
+  validates :rating, inclusion: { in: RANGE }
 end
