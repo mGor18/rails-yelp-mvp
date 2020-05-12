93922c4535b61ec36b0358e27939253d20732e6e
Matt Gordon <mattg1520@gmail.com>
2020-05-12 23:11:54 +0300

    added reviews for each restaurant on show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-  get 'reviews/create'
   resources :restaurants do
-    #resources :reviews do
-    #end
+    resources :reviews, only: [:new, :create]
   end
 end
-
-
-# localhost:3000/restaurant/10/reviews/2
